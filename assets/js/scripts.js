7759aab0303c80fae9a7364019aa7db37cbc24f8
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-08-16 21:06:01 +0200

    Generator app

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,27 @@
-import Alpine from 'alpinejs'
+// Advice generator code
+const result = document.getElementById('advice-text');
+const number = document.getElementById('advice-number');
+const button = document.getElementById('advice-generator');
 
-window.Alpine = Alpine
+button.addEventListener('click', function () {
+    getAdvice();
 
-Alpine.start()
+});
+
+// If i would like to have a different advice on page load i could use this
+// addEventListener('load', function () {
+//     getAdvice();
+// });
+
+function getAdvice() {
+    fetch('https://api.adviceslip.com/advice').then(response => {
+        return response.json();
+    }).then(adviceData => {
+        const adviceObject = adviceData.slip;
+        number.innerHTML = adviceObject.id;
+        result.innerHTML = adviceObject.advice;
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['./src/**/*.njk', './src/**/*.md',],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-cyan': 'hsl(193, 38%, 86%)',
+        'neon-green': 'hsl(150, 100%, 66%)',
+        'grayish-blue': 'hsl(217, 19%, 38%)',
+        'dark-grayish-blue': 'hsl(217, 19%, 24%)',
+        'dark-blue': 'hsl(218, 23%, 16%)',
+      },
+      fontFamily: {
+        sans: ['Manrope']
+      },
+      fontSize: {
+        'fm-quote': ['24px', '32.78px'],
+        'fm-quote-lg': ['28px', '38.25px'],
+      }
+    },
   },
   plugins: [],
 }
